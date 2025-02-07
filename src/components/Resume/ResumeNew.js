fe55568f9c8af0ce7ef0966d8566bae9269cb367
Uzair Riaz Farooqui <uzair.r.farooqui@gmail.com>
2025-02-08 01:17:39 +0500

    fix warnings

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import { AiOutlineDownload } from "react-icons/ai";
-import { pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../../Assets/../Assets/resume.pdf";
 import Experience from "./Experience";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   return (
