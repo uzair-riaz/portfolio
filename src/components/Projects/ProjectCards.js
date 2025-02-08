8b6bb72a14b042725775e2b65ce380f725a3a724
Uzair Riaz Farooqui <uzair.r.farooqui@gmail.com>
2025-02-08 16:37:40 +0500

    make card clickable

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,35 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Visit"}
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
+    <a href={props.demoLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+      <Card className="project-card-view" style={{ cursor: "pointer" }}>
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </a>
   );
 }
 export default ProjectCards;
